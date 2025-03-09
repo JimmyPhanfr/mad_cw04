43de657c5713e360106802437bfe1c5b1f5d061c
JimmyPhanfr <JimmyPhanfr@gmail.com>
2025-03-09 13:18:22 -0400

    working on dialog box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'database_helper.dart';
+import 'dart:async';
 
 final _databaseHelper = DatabaseHelper();
 Future<void> main() async {
@@ -34,16 +35,87 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  final _textControllerPlanName = TextEditingController();
+  final _textControllerPlanDetails = TextEditingController();
+  // final _textControllerPlanDate = TextEditingController();
+
+  DateTime selectedDate = DateTime.now();
+
   Future openPlanDialog() => showDialog(
     context: context, 
     builder: (context) => AlertDialog(
       title: Text('Create New Plan'),
-      // content: Textfield(
-
-      // )
-
+      content: StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Column(
+            children: [
+              TextField(
+                controller: _textControllerPlanName,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name',
+                  hintText: 'Name new plan',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _textControllerPlanName.clear();
+                    },
+                    icon: const Icon(Icons.clear),
+                  ),
+                ),
+              ),
+              TextField(
+                controller: _textControllerPlanDetails,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Details',
+                  hintText: 'Give plan details',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _textControllerPlanDetails.clear();
+                    },
+                    icon: const Icon(Icons.clear),
+                  ),
+                ),
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("${selectedDate.toLocal()}".split(' ')[0]),
+                    IconButton(
+                      onPressed: () {
+                        _selectDate(context, setState);
+                      },
+                    icon: const Icon(Icons.calendar_month),
+                  ),
+                  ],
+                )
+              ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     _selectDate(context, setState);
+                  //   },
+                  //   child: const Text('Select date'),
+                  // ),
+            ],
+          );
+        },
+      ),
     ),
   );
+
+  Future<void> _selectDate(BuildContext context, setState) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
